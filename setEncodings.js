d5a89a084c367b24bfbeec93ec0418e35551a848
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-11-20 18:40:53 -0800

    move encoding information to own file

---
+++ setEncodings.js
@@ -0,0 +1,48 @@
+const encodings = {
+    "ROT 13": rot13_transform,
+    "Semaphore": null,
+    "Braille": braille_transform,
+    "Number": number_transform
+}
+
+let encodingChoice = null;
+
+let currentPageNumber = null;
+let currentLineNumber = null;
+let currentEncodedLine = null;
+
+const setEncodingChoice = choice => {
+    encodingChoice = choice;
+
+    document.getElementById("egg-chooser").classList.toggle("hidden")
+    document.getElementById("subheading-instruction").classList.toggle("hidden")
+    document.getElementById("subheading-back").classList.toggle("hidden")
+    document.getElementById("encoded-text").classList.toggle("hidden")
+
+    if (encodings[choice])
+        {
+            // update subheading
+            document.getElementById("subheading-chosen").textContent = choice;
+
+            // set first line of text
+            currentPageNumber = 0;
+            currentLineNumber = 0;
+            currentEncodedLine = encodings[choice](text[currentPageNumber][currentLineNumber]);
+
+            document.getElementById("current-line").appendChild(currentEncodedLine);
+        }
+}
+
+const goBackToMenu = () => {
+    encodingChoice = null;
+
+    document.getElementById("subheading-instruction").classList.toggle("hidden")
+    document.getElementById("subheading-back").classList.toggle("hidden")
+    document.getElementById("encoded-text").classList.toggle("hidden");
+    document.getElementById("egg-chooser").classList.toggle("hidden");
+
+    let currentLine = document.getElementById("current-line");
+
+    while (currentLine.hasChildNodes())
+        { currentLine.removeChild(currentLine.childNodes[0]); }
+}
