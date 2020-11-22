4d4967bdb158560bfca8eed0288f19d86cba90ee
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-11-21 19:37:11 -0800

    dynamically create egg choices on main menu

---
+++ createEggChoices.js
@@ -0,0 +1,32 @@
+const eggChooser = document.getElementById("egg-chooser");
+
+for (let encoding in encodings)
+    {
+        let eggChoice = document.createElement("div");
+        eggChoice.classList.add("egg-choice");
+        eggChoice.setAttribute("onClick", `setEncodingChoice('${encoding}')`);
+
+        let egg = document.createElement("div");
+        egg.classList.add("egg");
+        egg.textContent = "egg";
+
+        let encodingText = document.createElement("h3");
+        encodingText.textContent = encoding;
+
+        eggChoice.appendChild(egg);
+        eggChoice.appendChild(encodingText);
+
+        eggChooser.appendChild(eggChoice);
+    }
+
+/**
+// Example:
+
+    <div class="egg-choice" onClick="setEncodingChoice('ROT 13')">
+        <div class="egg">
+            egg
+        </div>
+        <h3>ROT 13</h3>
+    </div>
+
+**/
