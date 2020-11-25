576d1eff337f4907323f149fcc6868f1fca8662a
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-11-24 17:08:31 -0800

    trim user input before processing

--- processText.js
+++ processText.js
@@ -2,7 +2,7 @@ let text = [];
 
 const updateText = () => {
 
-    let sourceText = document.getElementById("textarea-input").value;
+    let sourceText = document.getElementById("textarea-input").value.trim();
 
     let pages = sourceText.split("\n\n");
     text = pages.map(page => page.split("\n"));
